766324fa6c2b17d858aa4dafb4b0e7453ae06172
a30xp30 <92660592+a30xp30@users.noreply.github.com>
2021-12-20 16:28:22 -0600

    SIMULADOR de botnet para tiktok código de python
    Botnet para tiktok emulador python codigos :)

---
+++ emulador.py
@@ -0,0 +1,15 @@
+from time import sleep
+print("TIKTOK REPORT BOT ")
+sleep(2)
+print ("Procesando script")
+sleep(1)
+print("Accediendo a database")
+sleep (1)
+print ("proceaando server")
+sleep(1)
+print ("SCRIPT ACABADO :)")
+sleep (1)
+count = 0
+while count < 9999999999999999999999999999999999999999999999999:
+    print ("Bot for user nombre de el usuario tiktok", str(count))
+    count = count +1
